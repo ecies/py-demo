acf2efdee3a7eea6cf7fb52207c00cf1ae2b0dbe
Weiliang Li <to.be.impressive@gmail.com>
2025-07-14 00:16:42 +0900

    Add curve

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
 from typing import Optional
 
 from ecies import decrypt, encrypt
+from ecies.config import Config, EllipticCurve
 from litestar import Litestar, MediaType, Request, Response, post
 from litestar.enums import RequestEncodingType
 from litestar.openapi import OpenAPIConfig
 from litestar.openapi.datastructures import ResponseSpec
 from litestar.openapi.plugins import SwaggerRenderPlugin
 from litestar.params import Body
+from litestar.status_codes import HTTP_400_BAD_REQUEST
 from msgspec import Struct
 
 
-class RequestException(Exception):
-    def __init__(self, detail: str):
-        self.detail = detail
-
-
+# request and response
 class Payload(Struct):
     data: str
     prv: Optional[str] = None
     pub: Optional[str] = None
+    curve: EllipticCurve = "secp256k1"
 
 
 class RequestError(Struct):
     detail: str
 
 
+# exceptions
+class RequestException(Exception):
+    def __init__(self, detail: str):
+        self.detail = detail
+
+
+# exception handlers
+def request_exception_handler(_: Request, exc: RequestException) -> Response:
+    return Response(
+        media_type=MediaType.JSON,
+        content={"detail": exc.detail},
+        status_code=HTTP_400_BAD_REQUEST,
+    )
+
+
+# utils
 def make_readable(data: bytes):
     try:
         return data.decode()
@@ -32,37 +47,31 @@ def make_readable(data: bytes):
         return data.hex()
 
 
+# routes
 @post(
     "/",
     responses={
-        400: ResponseSpec(data_container=RequestError),
+        HTTP_400_BAD_REQUEST: ResponseSpec(data_container=RequestError),
     },
 )
 async def encrypt_decrypt(
     data: Payload = Body(media_type=RequestEncodingType.URL_ENCODED),
 ) -> str:
+    config = Config(elliptic_curve=data.curve)
     if data.prv and data.data:
         try:
-            decrypted = decrypt(data.prv, bytes.fromhex(data.data))
+            decrypted = decrypt(data.prv, bytes.fromhex(data.data), config)
             return make_readable(decrypted)
         except ValueError:
             raise RequestException(detail="Invalid private key or data")
     elif data.pub and data:
         try:
-            encrypted = encrypt(data.pub, data.data.encode())
+            encrypted = encrypt(data.pub, data.data.encode(), config)
             return encrypted.hex()
         except ValueError:
             raise RequestException(detail="Invalid public key or data")
     else:
         raise RequestException(detail="Invalid request")
-
-
-def request_exception_handler(_: Request, exc: RequestException) -> Response:
-    return Response(
-        media_type=MediaType.JSON,
-        content={"detail": exc.detail},
-        status_code=400,
-    )
 
 
 app = Litestar(
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,46 @@
+import pytest
+from ecies.config import EllipticCurve
 from litestar.status_codes import HTTP_201_CREATED
 from litestar.testing import AsyncTestClient
 
 
-async def test_encrypt(client: AsyncTestClient):
-    response = await client.post(
-        "/",
-        data={
-            "data": "abc",
-            "pub": "0x98afe4f150642cd05cc9d2fa36458ce0a58567daeaf5fde7333ba9b403011140a4e28911fcf83ab1f457a30b4959efc4b9306f514a4c3711a16a80e3b47eb58b",
-        },
-        headers={"Content-Type": "application/x-www-form-urlencoded"},
-    )
+@pytest.fixture(scope="session")
+def headers():
+    return {"Content-Type": "application/x-www-form-urlencoded"}
+
+
+@pytest.fixture(scope="session")
+def data():
+    return "abc🔒"
+
+
+@pytest.mark.parametrize("curve", [None, "secp256k1", "x25519", "ed25519"])
+async def test_encrypt(
+    client: AsyncTestClient, data, headers, curve: EllipticCurve | None
+):
+    if curve is None or curve == "secp256k1":
+        sk = "0x95d3c5e483e9b1d4f5fc8e79b2deaf51362980de62dbb082a9a4257eef653d7d"
+        pk = "0x98afe4f150642cd05cc9d2fa36458ce0a58567daeaf5fde7333ba9b403011140a4e28911fcf83ab1f457a30b4959efc4b9306f514a4c3711a16a80e3b47eb58b"
+    elif curve == "x25519":
+        sk = "0xddcfbccd11922e02d1afe6df9f79094d70aba9f7ee7b016fb2cf442f5ab4d2ed"
+        pk = "0x787e070a279b5a04df0b0388ec8dea22497505afa0f7453768ff56ba104c6126"
+    elif curve == "ed25519":
+        sk = "0xa8796a9360b6fac856a9b949066455c2233f5e8b628412c507633762acb94f93"
+        pk = "0x3b1c46d8b01e25d66d1484a87fcc49acf148e15e3f25bc2e65eda81dcdac3a19"
+    else:
+        raise NotImplementedError
+
+    encrypt_data = {"data": data, "pub": pk}
+    if curve:
+        encrypt_data["curve"] = curve
+
+    response = await client.post("/", data=encrypt_data, headers=headers)
+    assert response.status_code == HTTP_201_CREATED
+
+    decrypt_data = {"data": response.text, "prv": sk}
+    if curve:
+        decrypt_data["curve"] = curve
+
+    response = await client.post("/", data=decrypt_data, headers=headers)
+    assert response.text == data
     assert response.status_code == HTTP_201_CREATED
